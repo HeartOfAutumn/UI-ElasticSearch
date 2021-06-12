4be8e63c422b81c806fc7134ed3a9b5f6786aa37
Minh Thu Ngô <ngominhthuworks@gmail.com>
2021-06-12 13:41:23 +0700

    write guide for run project

--- src/screen/BookScreen.js
+++ src/screen/BookScreen.js
@@ -1,36 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import ListBook from '../components/ListBook';
 import Search from '../components/Search';
-import data from '../data';
 
 const BookScreen = (props) => {
-  const [books, setBooks] = useState(data.books);
+  // const [books, setBooks] = useState(data.books);
+  const [books, setBooks] = useState([]);
   const [searchBy, setSearchBy] = useState({});
 
-  // Work with API
+  // Mock API với json server
   useEffect(() => {
+    async function postData(url) {
+      const response = await fetch(url, {
+        method: 'GET',
+      });
+      return response.json();
+    }
 
+    postData('http://localhost:3000/books')
+      .then(data => {
+        setBooks(data);
+      });
   }, [searchBy])
 
   const matchBooks = (searchBy) => {
     setSearchBy(searchBy);
   }
 
-  // Test với dữ liệu ở máy
-  // const matchBooks = (searchBy) => {
-  // console.log(searchBy);
-  //   if (searchBy.text === "") {
-  //     setBooks(data.books);
-  //   } else {
-  //     let rightBooks = books.filter(
-  //       book =>
-  //         book.category.toLowerCase().includes(searchBy.text.toLowerCase())
-  //         || book.title.toLowerCase().includes(searchBy.text.toLowerCase())
-  //         || book.author.toLowerCase().includes(searchBy.text.toLowerCase())
-  //     );
-  //     setBooks(rightBooks);
-  //   }
-  // };
+  // Test với dữ liệu ở máys
+/*   const matchBooks = (searchBy) => {
+    console.log(searchBy);
+    if (searchBy.text === "") {
+      setBooks();
+    } else {
+      let rightBooks = books.filter(
+        book =>
+          book.category.toLowerCase().includes(searchBy.text.toLowerCase())
+          || book.title.toLowerCase().includes(searchBy.text.toLowerCase())
+          || book.author.toLowerCase().includes(searchBy.text.toLowerCase())
+      );
+      setBooks(rightBooks);
+    }
+  } */;
 
   return (
     <div className="container-fluid">
